20f8f2e6ab94483caa71b4d7a8647d42d35fff9e
francesco cini <frakorn@gmail.com>
2020-04-12 12:59:01 +0200

    send messages to all listeners

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 let express = require('express')
 let app = express();
-
 let http = require('http');
 let server = http.Server(app);
-
 let socketIO = require('socket.io');
 let io = socketIO(server);
-
 const port = process.env.PORT || 3000;
 
 io.on('connection', (socket) => {
     console.log('user connected');
-
     socket.on('new-message', (message) => {
       console.log(message);
-	  socket.emit('new-message', message);
+	  io.sockets.emit('new-message', message);
 });
 })
 
